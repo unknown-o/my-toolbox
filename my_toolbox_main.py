3d0d45dd669cb634997e97c970888a990f797e9f
UnknownO <i@mr-wu.top>
2022-01-29 07:07:25 +0000

    下架短链接生成功能

--- my_toolbox_main.py
+++ my_toolbox_main.py
@@ -187,13 +187,6 @@ class my_toolbox_main:
             f.write(hostsNew)
         return {'msg': '成功删除此条hosts', 'status': 1}
 
-    def createShortLink(self, args):
-        url = 'https://api.unknown-o.com/shorturl/'
-        nowTime = int(time.time())
-        data = {'key': hashlib.md5(('KagamineYes!'+str(nowTime)).encode("utf-8")).hexdigest(), 'timestamp': nowTime, 'type': "n", 'url': args.url}
-        response = json.loads(requests.post(url, data).text)
-        return {'status': 1, 'result': response['result']}
-
     def getExecuteResult(self, args):
         if(os.popen("echo $(ps -ef | grep '/www/temp.sh' | grep -v grep | awk '{print $2}')").read() == "\n"):
             if(os.path.exists("/www/server/panel/plugin/my_toolbox/tmp/executeCommand.tmp")):
